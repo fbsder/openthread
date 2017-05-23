79dd3f1fbb183146d5b3c3d25d151f2f498097eb
ldc <ldc@ldcdeMacBook-Pro.local>
2017-05-23 21:38:50 +0800

    merge upstream

--- .travis/script.sh
+++ .travis/script.sh
@@ -101,15 +101,6 @@ set -x
     arm-none-eabi-size  output/samr21/bin/ot-cli-mtd || die
     arm-none-eabi-size  output/samr21/bin/ot-ncp-ftd || die
     arm-none-eabi-size  output/samr21/bin/ot-ncp-mtd || die
-
-    git checkout -- . || die
-    git clean -xfd || die
-    ./bootstrap || die
-    COMMISSIONER=1 JOINER=1 DHCP6_CLIENT=1 DHCP6_SERVER=1 DNS_CLIENT=1 make -f examples/Makefile-kw41z || die
-    arm-none-eabi-size  output/kw41z/bin/ot-cli-ftd || die
-    arm-none-eabi-size  output/kw41z/bin/ot-cli-mtd || die
-    arm-none-eabi-size  output/kw41z/bin/ot-ncp-ftd || die
-    arm-none-eabi-size  output/kw41z/bin/ot-ncp-mtd || die
 }
 
 [ $BUILD_TARGET != arm-gcc54 ] || {
@@ -166,15 +157,6 @@ set -x
     arm-none-eabi-size  output/samr21/bin/ot-cli-mtd || die
     arm-none-eabi-size  output/samr21/bin/ot-ncp-ftd || die
     arm-none-eabi-size  output/samr21/bin/ot-ncp-mtd || die
-
-    git checkout -- . || die
-    git clean -xfd || die
-    ./bootstrap || die
-    COMMISSIONER=1 JOINER=1 DHCP6_CLIENT=1 DHCP6_SERVER=1 make -f examples/Makefile-kw41z || die
-    arm-none-eabi-size  output/kw41z/bin/ot-cli-ftd || die
-    arm-none-eabi-size  output/kw41z/bin/ot-cli-mtd || die
-    arm-none-eabi-size  output/kw41z/bin/ot-ncp-ftd || die
-    arm-none-eabi-size  output/kw41z/bin/ot-ncp-mtd || die
 }
 
 [ $BUILD_TARGET != arm-gcc63 ] || {
